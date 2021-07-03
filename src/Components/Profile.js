752506987d41eec1e8a6edb758e5f1c7e81c7e6b
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-07-02 22:32:57 -0400

    Refactored profile page to use hooks

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,48 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'reactstrap';
 import './Profile.css';
 
-class Profile extends Component {
-	UNSAFE_componentWillMount() {
-		this.setState({ profile: {} });
-		const { userProfile, getProfile } = this.props.auth;
+const Profile = props => {
+	const [profile, setProfile] = useState({});
+
+	const { userProfile, getProfile } = props.auth;
+
+	const fetchProfile = () => {
 		if (!userProfile) {
 			getProfile((err, profile) => {
-				this.setState({ profile });
+				setProfile(profile);
 			});
 		} else {
-			this.setState({ profile: userProfile });
+			setProfile(userProfile);
 		}
-	}
-	render() {
-		const { profile } = this.state;
-		return (
-			<div className="container">
-				<div className="profile-area">
-					<Choose>
-						<When condition={profile.name}>
-							<div>
-								<h1>Profile</h1>
-								<div><img src={profile.picture} alt="Profile" /></div>
-								<h3>{profile.name}</h3>
-								<p>Nickname: <em>{profile.nickname}</em></p>
-								<pre>{JSON.stringify(profile, null, 2)}</pre>
-							</div>
-						</When>
-						<Otherwise>
-							<div className="balls">
-								<div></div>
-								<div></div>
-								<div></div>
-							</div>
-						</Otherwise>
-					</Choose>
-				</div>
+	};
+
+	useEffect(() => {
+		fetchProfile();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<div className="container">
+			<div className="profile-area">
+				<Choose>
+					<When condition={profile.name}>
+						<div>
+							<h1>Profile</h1>
+							<div><img src={profile.picture} alt="Profile" /></div>
+							<h3>{profile.name}</h3>
+							<p>Nickname: <em>{profile.nickname}</em></p>
+							<pre>{JSON.stringify(profile, null, 2)}</pre>
+						</div>
+					</When>
+					<Otherwise>
+						<div className="balls">
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
+					</Otherwise>
+				</Choose>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 Profile.propTypes = {
 	auth: PropTypes.object,
